810e0a4022a4cc03eeaace5e47890b15b338d357
vignesh <104735393+ghosthanos@users.noreply.github.com>
2023-06-08 00:14:42 +0530

    Add files via upload

---
+++ lib/screens/appointmentdone.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppointmentDone extends StatelessWidget {
+  const AppointmentDone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(87, 193, 142, 1),
+      body: Container(
+        child: Align(
+          alignment: Alignment.center,
+          child: SizedBox(
+            height: 50,
+            child: Text(
+              "Your Appointment has been booked!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+          // Add your onPressed code here!
+        },
+        backgroundColor: Color.fromARGB(224, 0, 128, 255),
+        child: const Icon(
+          Icons.arrow_back_ios_new,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+    );
+  }
+}
